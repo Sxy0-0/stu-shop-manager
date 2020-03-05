c3ef115a1bb0ce965b24ae2cba0b7d4c33ff2232
sxy <1094093339@qq.com>
2020-03-05 16:44:12 +0800

    init manager

---
+++ src/components/Frame/Index.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+// import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import {adminRoutes} from '../../routes';
+import logo from './logo.png';
+
+const { Header, Content, Sider } = Layout;
+const routes = adminRoutes.filter(route => route.isShow)
+
+class Index extends Component {
+    render() {
+        return (
+            <Layout>
+                <Header className="header">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    {/*<Menu*/}
+                    {/*    theme="dark"*/}
+                    {/*    mode="horizontal"*/}
+                    {/*    defaultSelectedKeys={['2']}*/}
+                    {/*    style={{ lineHeight: '64px' }}*/}
+                    {/*>*/}
+                    {/*    <Menu.Item key="1">nav 1</Menu.Item>*/}
+                    {/*    <Menu.Item key="2">nav 2</Menu.Item>*/}
+                    {/*    <Menu.Item key="3">nav 3</Menu.Item>*/}
+                    {/*</Menu>*/}
+                </Header>
+                <Layout>
+                    <Sider width={200} className="site-layout-background">
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{ height: '100%', borderRight: 0 }}
+                        >
+                            {routes.map(route => {
+                                return (
+                                    <Menu.Item key = {route.path}>{route.title}</Menu.Item>
+                                )
+                            })}
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Content
+                            className="site-layout-background"
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280,
+                                backgroundColor:"white"
+                            }}
+                        >
+                            {this.props.children},1
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+export default Index;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import "antd/dist/antd.css";
+import { HashRouter as Router,Switch, Route} from 'react-router-dom'
+import {mainRoutes} from './routes'
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route path="/admin" render={routeProps => <App {...routeProps} />} />
+            {mainRoutes.map(route => {
+                // return <Route key={route.path} path={route.path} component={route.component}/>
+                return <Route key={route.path} {...route}/>
+            })}
+        </Switch>
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
